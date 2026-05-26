f8577a35ec5f81be388b4f97d76569377b471598
Masila-Isaac <masilaisaacmim@gmail.com>
2026-05-26 13:48:56 +0300

    notification implementation

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -3,7 +3,6 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter/material.dart';
 import '../services/chat_service.dart';
 
-/// Handles FCM push notifications for incoming messages.
 /// Call NotificationService.instance.init() from main.dart after Firebase.initializeApp().
 class NotificationService {
   NotificationService._();
